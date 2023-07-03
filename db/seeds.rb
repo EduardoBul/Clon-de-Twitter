3651344f77f1a06e80967b221684833fda4368c2
EduardoBul <carilao.bulnes@gmail.com>
2023-07-02 22:15:15 -0400

    quinto commit añadiendo 35 registros para posterior uso en Pagy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+35.times do |i|
+    @tweets = Tweet.create(description: "Tweet #{i + 1}", username: "Eduardo #{i + 1}")
+
+    print @tweets
+end
