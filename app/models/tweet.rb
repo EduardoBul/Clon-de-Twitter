1c8d6297bf6c0a529421abec354e6c918f711ae0
EduardoBul <carilao.bulnes@gmail.com>
2023-07-02 20:01:51 -0400

    segundo commit de clon twitter

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,8 @@
 class Tweet < ApplicationRecord
-end
+    include PgSearch::Model
+    pg_search_scope :search_full_text, 
+    against: {
+        description: 'A',
+        id: 'B',
+      }
+    end
